25f1138c819834539632458123783ea84ce51777
dodolereporter <dodolereporter@gmail.com>
2023-06-24 23:53:10 +0200

    Check OS

--- gameStart.js
+++ gameStart.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process');
 const fs = require('fs');
+const { isWindows, isMac, isLinux } = require('./utils/checkOs');
 
 let gamePaths;
 if (fs.existsSync('gamePaths.json')) {
@@ -8,8 +9,16 @@ if (fs.existsSync('gamePaths.json')) {
     gamePaths = {};
 }
 
-let currentGamePath = '/Users/enzodeshayes/Desktop/MP';
-let currentGame = '';
+let currentGamePath = 'C:\\Program Files\\COD1';
+
+let startCmd = '';
+
+if (isMac) {
+    startCmd = 'wine64 CoDMP.exe';
+} else if (isWindows) {
+    startCmd = 'CoDMP.exe';
+}
+
 
 function startLoading() {
     const button = document.querySelector(".button-play");
@@ -28,7 +37,7 @@ document.querySelector('.button-play').addEventListener('click', () => {
 
     startLoading();
 
-    exec('wine64 CoDMP.exe +connect 127.0.0.1', { cwd: currentGamePath }, (error, stdout, stderr) => {
+    exec(startCmd+' +connect 127.0.0.1', { cwd: currentGamePath }, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
